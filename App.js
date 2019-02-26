351d8b6da28d5f5e36c53274f088076a9518b068
mannix-lei <zx2351634862@outlook.com>
2019-02-26 10:14:31 +0800

    update

--- App.js
+++ App.js
@@ -1,25 +1,86 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View, TextInput } from 'react-native';
-import { white } from 'ansi-colors';
+import { View, Text, ScrollView,  }  from 'react-native';
+import { NavBar, Icon, Button } from 'antd-mobile-rn';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-export default class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: ''
-    }
-  }
-
+class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={{ backgroundColor: white, width: '100%', height: '100%' }}>
-        <TextInput style={{height: 40}} placeholder="Type here" onChangeText={(text) => this.setState({ text })} />
-        <Text style={{ padding: 10, fontSize: 42 }} >
-          {this.state.text.split(' ').map(word => word && '**').join(' ')}
-        </Text>
+      <View style={{ fles: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>this is home screen</Text>
+        <Text>Home Screen</Text>
+        <Button title="uuu" onClick={() => this.props.navigation.navigate('Details')}>FOO</Button>
       </View>
     );
   }
 }
 
+class DetailsScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button title="uuu" onClick={() => this.props.navigation.navigate('third')}>BAR</Button>
+      </View>
+    )
+  }
+}
+
+class ThirdScreen extends Component {
+  render() {
+    return (
+      <View style={{ backgroundColor: 'yellow', width: '100%', height: '100%' }}>
+        <Text>this is third screen.</Text>
+      </View>
+    )
+  }
+}
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
+    third: ThirdScreen
+  },
+  {
+    initialRouteName: 'Home'
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+export default class App extends Component {
+  render() {
+    return <AppContainer />
+  }
+}
+// // export default class HelloWorldApp extends Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+
+//     }
+//   }
+
+
+//   render() {
+//     return (
+//       <ScrollView style={{ backgroundColor: 'white', width: '100%', height: '100%' }}>
+//         <View>
+//         {/* <NavBar
+//             mode="light"
+//             icon={<Icon type="left"/>}
+//             onLeftClick={() => console.info('onLeftClick')}
+//             rightContent={[
+//               <Icon key="0" type="search" style={{ marginRight: '16px'}} />,
+//               <Icon key="1" type="ellipsis" />
+//             ]}>
+//         </NavBar> */}
+//         <Text style={{ fontSize: 20, textAlign: 'center', }}>Mannix_Lei</Text>
+//         <Button>Click me!</Button>
+//       </View>
+//       </ScrollView>
+//     )
+//   }
+// }
